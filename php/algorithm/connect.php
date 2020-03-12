a35c21a65f49a835469035b64da4e8d4e41f14cc
truongluu1102 <49853294+truongluu1102@users.noreply.github.com>
2020-03-12 11:41:46 +0700

    connect cloud database

--- php/algorithm/connect.php
+++ php/algorithm/connect.php
@@ -5,7 +5,7 @@ class connect
 	public $connect;
 	public function connect()
 	{
-		$this->connect=mysqli_connect("127.0.0.1", "root", "", "teamnd");
+		$this->connect=mysqli_connect("us-cdbr-iron-east-04.cleardb.net", "b2a25855721aa", "a87e1f6f", "heroku_12ce267cd554a88");
 	}
 	public function close()
 	{
