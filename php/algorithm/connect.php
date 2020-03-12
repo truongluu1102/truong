971a5dfc389b5e3707e01170173ad87f05fc2212
truongluu1102 <49853294+truongluu1102@users.noreply.github.com>
2020-03-12 11:44:18 +0700

    connect 2nd

--- php/algorithm/connect.php
+++ php/algorithm/connect.php
@@ -5,7 +5,7 @@ class connect
 	public $connect;
 	public function connect()
 	{
-		$this->connect=mysqli_connect("us-cdbr-iron-east-04.cleardb.net", "b2a25855721aa", "a87e1f6f", "heroku_12ce267cd554a88");
+		$this->connect=mysqli_connect("us-cdbr-iron-east-04.cleardb.net", "b2a25855721aac", "a87e1f6f", "heroku_12ce267cd554a88");
 	}
 	public function close()
 	{
